e5b4745726bffea8f051c5d8ee948be068aa6ff0
Berkay Uysak <77589774+uysak@users.noreply.github.com>
2023-05-27 02:00:32 +0300

    Entity classes have been created

---
+++ Entities/Concrete/Brand.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Brand : IEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Url)]
+        public string WebsiteURL { get; set; }
+
+    }
+}
---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Car
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+
+        [Required]
+        [Column(TypeName ="smallint")]
+        public int BrandId { get; set; }
+        
+
+        [Required]
+        [StringLength(30)]
+        public string Model { get; set; }
+
+        [Required]
+        [Column(TypeName = "smallint")]
+        public ushort ModelYear { get; set; }
+
+
+        [ForeignKey("BrandId")]
+        public Brand Brand { get; set; }
+
+    }
+}
